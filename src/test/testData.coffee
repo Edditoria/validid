914d79a18c78a4014d63ebeec9150c5c6f97a6d3
Edditoria <edditoria@gmail.com>
2017-02-28 00:53:59 +0800

    [test] Add more test data; Fix the name window.testData in testData

--- src/test/testData.coffee
+++ src/test/testData.coffee
@@ -2,9 +2,19 @@ testData = [
   {'id': 'G123456A', cardType: 'hkid', expect: true}
   {'id': 'A5555550', cardType: 'hkid', expect: true}
   {'id': 'AB9876542', cardType: 'hkid', expect: true}
+  {'id': 'A123456789', cardType: 'twid', expect: true}
+  {'id': 'M115187864', cardType: 'twid', expect: true}
+  {'id': 'Z256783650', cardType: 'twid', expect: true}
+  {'id': '11010120170210193X', cardType: 'cnid', expect: true}
+  {'id': '310101200001013948', cardType: 'cnid', expect: true}
+  {'id': '120103198806018241', cardType: 'cnid', expect: true}
+  {'id': '120103198806018241', cardType: 'cnid', expect: true}
+  {'id': '781030-5668081', cardType: 'krid', expect: true}
+  {'id': '951103-5438151', cardType: 'krid', expect: true}
+  {'id': '700623-2711917', cardType: 'krid', expect: true}
 ]
 
 if module? and module.exports
   module.exports = testData
 if window?
-  window.validid = testData
+  window.testData = testData
--- test/testData.js
+++ test/testData.js
@@ -15,6 +15,46 @@
       'id': 'AB9876542',
       cardType: 'hkid',
       expect: true
+    }, {
+      'id': 'A123456789',
+      cardType: 'twid',
+      expect: true
+    }, {
+      'id': 'M115187864',
+      cardType: 'twid',
+      expect: true
+    }, {
+      'id': 'Z256783650',
+      cardType: 'twid',
+      expect: true
+    }, {
+      'id': '11010120170210193X',
+      cardType: 'cnid',
+      expect: true
+    }, {
+      'id': '310101200001013948',
+      cardType: 'cnid',
+      expect: true
+    }, {
+      'id': '120103198806018241',
+      cardType: 'cnid',
+      expect: true
+    }, {
+      'id': '120103198806018241',
+      cardType: 'cnid',
+      expect: true
+    }, {
+      'id': '781030-5668081',
+      cardType: 'krid',
+      expect: true
+    }, {
+      'id': '951103-5438151',
+      cardType: 'krid',
+      expect: true
+    }, {
+      'id': '700623-2711917',
+      cardType: 'krid',
+      expect: true
     }
   ];
 
@@ -23,7 +63,7 @@
   }
 
   if (typeof window !== "undefined" && window !== null) {
-    window.validid = testData;
+    window.testData = testData;
   }
 
 }).call(this);
