7df78a6c6e4451cf42f6461528ed548247cd0c88
Edditoria <edditoria@gmail.com>
2020-01-20 17:38:29 +0800

    Change "main" from <dist/> to <lib/> for bower

--- build.sh
+++ build.sh
@@ -2,7 +2,6 @@
 
 echo "Compile coffeescript files..."
 npx coffee --transpile --output ./lib/ --compile ./src/*.coffee
-npx coffee --transpile --output ./dist/ --compile ./src/*.coffee
 npx coffee --transpile --output ./test/ --compile ./src/test/*.coffee
 npx coffee --transpile --output ./test/ --compile ./src/*.coffee
 echo "Compile finish."
