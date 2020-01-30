1b5e8a4dc86b2c6fa122a9f4626daddf0d47a4a7
Edditoria <edditoria@gmail.com>
2020-01-30 14:44:59 +0800

    Update package files; Remove outdated files

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-echo "Compile coffeescript files..."
-npx coffee --transpile --output ./lib/ --compile ./src/*.coffee
-npx coffee --transpile --output ./test/ --compile ./src/test/*.coffee
-npx coffee --transpile --output ./test/ --compile ./src/*.coffee
-echo "Compile finish."
