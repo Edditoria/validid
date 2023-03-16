631275ff1d948f43e7ced00b781fe34ccee24abd
Edditoria <edditoria@pm.me>
2023-03-17 03:44:09 +0800

    Decaffeinate: gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+const gulp = require('gulp');
+const coffee = require('gulp-coffee');
+const prettier = require('gulp-prettier');
+const replace = require('gulp-replace');
+const rename = require('gulp-rename');
+const del = require('del');
+
+const srcGlobs = ['src/**/*coffee', '!src/test/**'];
+const delGlobs = ['bundles/**', 'esm/**', 'test/**'];
+
+// $1: Extension name to be replaced; $2: Two characters "';" to be reserved
+const regex = /(.coffee)(['"];\s*)$/gm;
+const extname = '.mjs';
+
+function buildESM(cb) {
+	gulp
+		.src(srcGlobs)
+		.pipe(coffee())
+		.pipe(
+			prettier({
+				editorconfig: true,
+				singleQuote: true,
+			})
+		)
+		.pipe(replace(regex, extname + '$2'))
+		.pipe(rename({ extname }))
+		.pipe(gulp.dest('esm/'));
+	cb();
+}
+
+function clean(cb) {
+	del(delGlobs);
+	cb();
+}
+
+exports.buildESM = buildESM;
+exports.clean = clean;
