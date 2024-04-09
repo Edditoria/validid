d86491652b2d77cc98a991b229abc2e9642568cd
Edditoria <edditoria@pm.me>
2024-04-09 19:36:39 +0800

    Update prettier config

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -1,8 +1,17 @@
+/**
+ * @note
+ * Prettier, eslint, .editorconfig and .gitattributes work together.
+ * Remember to check their configs regularly.
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import('prettier').Config}
+ */
 export default {
-	useTabs: true,
-	singleQuote: true,
-	jsxSingleQuote: false, // as default.
-	trailingComma: 'es5', // as default.
-	bracketSpacing: true, // as default.
 	printWidth: 999,
+	useTabs: true, // also in .editorconfig file.
+	semi: true, // as default.
+	singleQuote: true,
+	jsxSingleQuote: true,
+	trailingComma: 'es5',
+	proseWrap: 'preserve', // as default.
+	endOfLine: 'lf', // as default. Also in .editorconfig and .gitattributes files.
 };
