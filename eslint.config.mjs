e7a2e1cff3653fbe9ad0dd6c78eee8d10c709594
Edditoria <edditoria@pm.me>
2024-04-09 21:30:17 +0800

    npm update eslint to v9 and migrate configs...
    Did not change configs, tho.
    Ignore the deprecated rules in the meantime.

---
+++ eslint.config.mjs
@@ -0,0 +1,33 @@
+import eslintConfigPrettier from 'eslint-config-prettier';
+import globals from 'globals';
+import js from '@eslint/js';
+
+export default [
+	js.configs.recommended,
+	eslintConfigPrettier,
+	{
+		languageOptions: {
+			ecmaVersion: 2021, // aka 12.
+			sourceType: 'module',
+			globals: { ...globals.es2021, ...globals.node },
+		},
+		rules: {
+			'linebreak-style': ['error', 'unix'], // as default in ESLint.
+			indent: ['error', 'tab', { SwitchCase: 1 }],
+			quotes: ['error', 'single'],
+			'jsx-quotes': ['error', 'prefer-double'], // as default in ESLint.
+			'max-len': [
+				'warn',
+				{
+					code: 80,
+					ignoreTrailingComments: true,
+					ignoreUrls: true,
+					ignoreStrings: true,
+					ignoreTemplateLiterals: true,
+					ignoreRegExpLiterals: true,
+				},
+			],
+			semi: ['error', 'always'], // as default in ESLint.
+		},
+	},
+];
