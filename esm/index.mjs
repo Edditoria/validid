4e789bc471e1cfdec3ca91c3da1c6882d263a737
Edditoria <edditoria@pm.me>
2024-06-16 19:35:33 +0800

    Deprecate everything in index.mjs, for now

--- esm/index.mjs
+++ esm/index.mjs
@@ -5,11 +5,17 @@ import twid from './twid.mjs';
 import twrc from './twrc.mjs';
 import twrcLegacy from './twrc-legacy.mjs';
 
-import { normalize } from './utils/normalize.mjs';
+import normalize from './utils/normalize.mjs';
 import isDateValid from './utils/is-date-valid.mjs';
 import getMaxDate from './utils/get-max-date.mjs';
 import getTwrcFormat from './utils/get-twrc-format.mjs';
 import isTwidChecksumValid from './utils/is-twid-checksum-valid.mjs';
+
+/**
+ * @file This file will be removed eventually.
+ * Please update your code ASAP.
+ * For details, read the `@deprecated` note in each function that you are using.
+ */
 
 /**
  * @deprecated To be removed without notice. Please update your code ASAP.
